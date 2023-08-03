8ce4b4550ba484d21bd9fc7fcfdb60e296f0a217
Adrian Wachana <adrianwafula403@gmail.com>
2023-08-03 04:37:25 -0700

    Just as in task #0, we’d like you to automate the task of creating a custom HTTP header response, but with Puppe

---
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -0,0 +1,39 @@
+# 2-puppet_custom_http_response_header.pp
+
+# Install Nginx package
+package { 'nginx':
+  ensure => installed,
+}
+
+# Define a custom fact to retrieve the server's hostname
+Facter.add('server_hostname') do
+  setcode 'hostname'
+end
+
+# Configure Nginx with custom HTTP header
+file { '/etc/nginx/sites-available/default':
+  ensure  => file,
+  content => "# custom HTTP RESPONSE HEADER\n
+              server {\n
+                listen 80 default_server;\n
+                server_name _;\n
+                add_header X-Served-By $server_hostname;\n
+                # ... other Nginx configuration ...\n
+              }",
+  require => Package['nginx'],
+}
+
+# Create a symbolic link to enable the site
+file { '/etc/nginx/sites-enabled/default':
+  ensure => link,
+  target => '/etc/nginx/sites-available/default',
+  require => File['/etc/nginx/sites-available/default'],
+}
+
+# Ensure Nginx service is running and enabled
+service { 'nginx':
+  ensure  => running,
+  enable  => true,
+  require => [Package['nginx'], File['/etc/nginx/sites-enabled/default']],
+}
+
